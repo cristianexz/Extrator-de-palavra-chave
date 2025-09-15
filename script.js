5da34273f1a8bb4988789be08a26b2e99485dab3
cristianexz <chaves.cristiane@escola.pr.gov.br>
2025-09-15 08:45:36 -0300

    hfdrg

--- script.js
+++ script.js
@@ -3,21 +3,17 @@ const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
 botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
 
 function mostraPalavrasChave() {
+  const texto = document.querySelector("#entrada-de-texto").value;
 
-const texto = document.querySelector("#entrada-de-texto").value;
+  const campoResultado = document.querySelector("#resultado-palavrachave");
 
-const campoResultado = document.querySelector("#resultado-palavrachave");
+  const palavrasChave = processaTexto(texto);
 
-const palavrasChave = processaTexto(texto);
-
-campoResultado.textContent = palavrasChave.join(", ");
-
+  campoResultado.textContent = palavrasChave.join(", ");
 }
 
 function processaTexto(texto) {
+  let palavras = texto.split(/\P{L}+/u); 
 
-let palavras = texto.split(/\P{L}+/u);
-
-return palavras;
-
+  return palavras;
 }
